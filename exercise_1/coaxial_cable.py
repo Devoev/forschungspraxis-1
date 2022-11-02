04316d0b9c1869c4ec36f64fa71fe5ae2a861b4e
Devin Balian <devinbalian@gmail.com>
2022-11-02 19:29:28 +0100

    Updated h_field

--- exercise_1/coaxial_cable.py
+++ exercise_1/coaxial_cable.py
@@ -2,6 +2,8 @@ from typing import Final
 
 import matplotlib.pylab as plt
 import numpy as np
+import numpy.typing
+from numpy.typing import ArrayLike
 from scipy import constants as const
 
 l_z: Final[float] = 300e-3
@@ -13,15 +15,15 @@ eps_s: Final[float] = const.epsilon_0
 sig_cu: Final[float] = 57.7e6
 
 
-def h_field(r) -> float:
+def h_field(r: ArrayLike) -> float:
     """The h field of the coaxial cable"""
 
+    r = np.asarray(r)
     if r.any() < 0:
         raise ValueError("r must not be negative")
 
-    r01 = r <= r1
-    r12 = np.logical_and(r1 < r, r <= r2)
-    return r01 * __h_field_1__(r) + r12 * __h_field_2__(r)
+    in1 = r <= r1
+    return in1 * _h_field_1(r) + ~in1 * _h_field_2(r)
 
 
 def plot_h_field():
@@ -32,11 +34,11 @@ def plot_h_field():
     plt.show()
 
 
-def __h_field_1__(r) -> float:
+def _h_field_1(r) -> float:
     """The h field inside the conductor"""
     return I / (2 * np.pi * r1 ** 2) * r
 
 
-def __h_field_2__(r) -> float:
+def _h_field_2(r) -> float:
     """The h field inside the insulator"""
     return I / (2 * np.pi * r)
