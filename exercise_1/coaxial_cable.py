961f8e1efa9aa1971720d68ec10d58836a2512af
Devin Balian <devinbalian@gmail.com>
2022-11-04 14:26:12 +0100

    Added arg_as_array.py decorator Fixed h field inside insulator

--- exercise_1/coaxial_cable.py
+++ exercise_1/coaxial_cable.py
@@ -1,3 +1,4 @@
+import math
 from typing import Final
 
 import matplotlib.pylab as plt
@@ -5,6 +6,8 @@ import numpy as np
 import numpy.typing
 from numpy.typing import ArrayLike
 from scipy import constants as const
+
+from util.arg_as_array import arg_as_array
 
 l_z: Final[float] = 300e-3
 I: Final[float] = 16
@@ -15,10 +18,15 @@ eps_s: Final[float] = const.epsilon_0
 sig_cu: Final[float] = 57.7e6
 
 
+@arg_as_array()
 def h_field(r: ArrayLike) -> float:
-    """The h field of the coaxial cable"""
+    """
+    The h field of the coaxial cable
 
-    r = np.asarray(r)
+    :param r: Radius values.
+    :return: Values of the magnetic field strength H.
+    """
+
     if r.any() < 0:
         raise ValueError("r must not be negative")
 
@@ -34,11 +42,27 @@ def plot_h_field():
     plt.show()
 
 
-def _h_field_1(r) -> float:
-    """The h field inside the conductor"""
+@arg_as_array()
+def _h_field_1(r: ArrayLike) -> float:
+    """The h field inside the conductor
+
+    :param r: Radius values.
+    :return: Values of the magnetic field strength H inside the conductor.
+    """
     return I / (2 * np.pi * r1 ** 2) * r
 
 
-def _h_field_2(r) -> float:
-    """The h field inside the insulator"""
+@arg_as_array()
+def _h_field_2(r: ArrayLike) -> float:
+    """The h field inside the insulator. If r=0, the field strength will be nan.
+
+    :param r: Radius values.
+    :return: Values of the magnetic field strength H inside the insulator.
+    """
+
+    r = np.asarray(
+        list(
+            map(lambda x: x if x > 0 else math.nan, r)
+        )
+    )
     return I / (2 * np.pi * r)
---
+++ util/arg_as_array.py
@@ -0,0 +1,22 @@
+from typing import Callable
+
+import numpy as nd
+
+
+def arg_as_array(i=0):
+    """ Converts the i-th argument to a numpy array.
+
+    :param i: The index of the argument. By default, the first argument (0 index).
+    :return: The modified function.
+    """
+
+    def _to_array(fun):
+        def call(*args, **kwargs):
+            res = list(args)
+            res[i] = nd.asarray(args[i])
+            res = tuple(res)
+            return fun(*res, **kwargs)
+
+        return call
+
+    return _to_array
