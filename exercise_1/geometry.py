cbd80712fbaa0a939a38d0032e42a5201137d6ab
Devin Balian <devinbalian@gmail.com>
2022-11-18 15:04:24 +0100

    Updated docs Created plot_geometry function

--- exercise_1/geometry.py
+++ exercise_1/geometry.py
@@ -3,6 +3,7 @@ from typing import Tuple
 import gmsh
 import numpy as np
 import numpy.typing
+from matplotlib import pyplot as plt
 
 from exercise_1.constants import l_z, r1, r2
 from util.gmsh import model
@@ -40,10 +41,29 @@ def cable() -> Tuple[int, int, int]:
     return wire, shell, gnd
 
 
+def plot_geometry():
+    """
+    Plots the cable geometry and visualizes the physical groups.
+    """
+    wire, shell, gnd = cable()
+    xw, yw, zw = node_coords(2, wire)
+    xs, ys, zs = node_coords(2, shell)
+    xg, yg, _ = node_coords(1, gnd)
+
+    # Creating plot
+    ax = plt.axes(projection='3d')
+    ax.plot_trisurf(xw, yw, zw, color="blue")
+    ax.plot_trisurf(xs, ys, zs, color="green", alpha=0.5)
+    ax.plot(xg, yg, color="red")
+
+    # show plot
+    plt.show()
+
+
 def node_coords(dim=-1, tag=-1) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Returns the coordinates of the nodes of the mesh.
-    :return The x,y,z coordinates.
+    :return: The x,y,z coordinates.
     """
 
     node_tags, nodes = msh.get_nodes_for_physical_group(dim, tag)
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 import gmsh
-import numpy as np
-from matplotlib import pyplot as plt
 
-from exercise_1.h_field import plot_h_field
-from exercise_1.geometry import cable, node_coords
+from exercise_1.geometry import plot_geometry
 
 msh = gmsh.model.mesh
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    wire, shell, gnd = cable()
-    xw, yw, zw = node_coords(2, wire)
-    xs, ys, zs = node_coords(2, shell)
-    xg, yg, _ = node_coords(1, gnd)
-
-    fig = plt.figure(figsize=(14, 9))
-    ax = plt.axes(projection='3d')
-
-    # Creating plot
-    ax.plot_trisurf(xw, yw, zw, color="blue")
-    ax.plot_trisurf(xs, ys, zs, color="green", alpha=0.5)
-    ax.plot(xg, yg, color="red")
-
-    # show plot
-    plt.show()
+    plot_geometry()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
