2cf7167eddceefee7410905b0bd771b39ea4c64a
Devin Balian <devinbalian@gmail.com>
2022-11-25 19:56:55 +0100

    Removed duplicate constants

--- exercise_1/h_field.py
+++ exercise_1/h_field.py
@@ -9,14 +9,6 @@ from scipy import constants as const
 
 from util.array import arg_as_array
 
-l_z: Final[float] = 300e-3
-I: Final[float] = 16
-r1: Final[float] = 2e-3
-r2: Final[float] = 3.5e-3
-mu_s: Final[float] = 5 * const.mu_0
-eps_s: Final[float] = const.epsilon_0
-sig_cu: Final[float] = 57.7e6
-
 
 @arg_as_array()
 def h_field(r: ArrayLike) -> float:
