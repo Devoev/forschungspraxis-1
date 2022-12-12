cfe7c39f7634442b1389fd2f37e5eb726ef26250
devin <devinbalian@gmail.com>
2022-12-12 18:54:53 +0100

    Added options argument to gmsh model function

--- util/gmsh.py
+++ util/gmsh.py
@@ -1,23 +1,39 @@
 import sys
 from functools import wraps
+from typing import Dict, Final
 
 import gmsh
 
+DefaultOptions: Final[Dict[str, float]] = {
+    # "General.Verbosity": 0,
+    "Mesh.MeshSizeFactor": 1,
+    # "Mesh.MshFileVersion": 2.2
+}
+"""The default options for gmsh initialization."""
 
-def model(name: str, dim: int = 3, show_gui: bool = False, finalize: bool = False):
+
+def model(name: str, dim: int = 3, show_gui: bool = False, finalize: bool = False, options: Dict[str, float] = None):
     """Indicates that the function generates a gmsh model.
 
     :param name The name of the gmsh model.
     :param dim: Dimension of the mesh.
     :param show_gui: Whether to show the gui.
     :param finalize: Whether to finalize the gmsh API.
+    :param options: Options that get passed to 'gmsh.option.set_number'.
     """
+
+    if options is None:
+        options = DefaultOptions
 
     def _model(func):
 
         @wraps(func)
         def wrapper(*args, **kwargs):
             gmsh.initialize()
+
+            for option, value in options.items():
+                gmsh.option.set_number(option, value)
+
             gmsh.model.add(name)
 
             res = func(*args, **kwargs)
@@ -34,4 +50,5 @@ def model(name: str, dim: int = 3, show_gui: bool = False, finalize: bool = Fals
             return res
 
         return wrapper
+
     return _model
