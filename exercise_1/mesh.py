d1ed42707a80b549880a7c4902b5b22bdf3e910a
devin <devinbalian@gmail.com>
2022-12-24 13:24:49 +0100

    Updated mesh.py and mssolution.py to use cached_property

--- exercise_1/mesh.py
+++ exercise_1/mesh.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 from typing import List, Dict, Tuple
 
 import gmsh
@@ -22,12 +23,12 @@ class Mesh:
     node_tags_elements: np.ndarray
     node_tags_groups: np.ndarray
 
-    @property
+    @cached_property
     def num_node(self) -> int:
         """The number of nodes."""
         return int(len(self.node_data) / 3)
 
-    @property
+    @cached_property
     def node_coords(self) -> np.ndarray:
         """The list of node coordinates."""
 
@@ -37,7 +38,7 @@ class Mesh:
         node = node[:, 0:2]
         return node
 
-    @property
+    @cached_property
     def node_tags(self) -> np.ndarray:
         """A list of node tags. Node tags of gmsh -1"""
         # ID of nodes
@@ -46,7 +47,7 @@ class Mesh:
         np.put_along_axis(self.node_coords, np.c_[node_tag, node_tag], self.node_coords, axis=0)
         return node_tag
 
-    @property
+    @cached_property
     def nodes(self) -> Dict[int, Point2D]:
         """A node tag-coord dict."""
         return dict(zip(self.node_tags, self.node_coords))
@@ -58,35 +59,35 @@ class Mesh:
         """
         return np.where(self.node_tags_groups[:, tag])[0]
 
-    @property
+    @cached_property
     def ind_elements(self) -> np.ndarray:
         """The indices of triangle elements."""
         return np.where(self.elementTypes == 2)[0]
 
-    @property
+    @cached_property
     def elem_tags(self) -> np.ndarray:
         """The tags of all triangle elements. **Not** necessarily starting from zero."""
         return self.element_tags[self.ind_elements[0]]
 
-    @property
+    @cached_property
     def elem_nodes(self) -> np.ndarray:
         """A vector of all nodes forming a triangle element.
         Shape: (e11,e12,e13,e21,e22,e23,...)
         """
         return np.array(self.node_tags_elements[self.ind_elements[0]])
 
-    @property
+    @cached_property
     def num_elems(self) -> int:
         """The number of elements."""
         return int(len(self.elem_nodes) / 3)
 
-    @property
+    @cached_property
     def elems_dict(self) -> Dict[int, np.ndarray]:
         """**DEPRECATED** A element tag-node tag dict."""
         node_tags = np.array_split(self.elem_nodes, self.num_elems)
         return dict(zip(self.elem_tags, node_tags))
 
-    @property
+    @cached_property
     def elems(self) -> np.ndarray:
         """A matrix of elements. Each row contains the node tags of the element vertices."""
 
@@ -98,14 +99,14 @@ class Mesh:
         elem_to_node = elem_to_node.astype('int')
         return elem_to_node
 
-    @property
+    @cached_property
     def edges(self) -> np.ndarray:
         """A matrix of edges. Each row contains the start and end node tags."""
         return np.r_[self.elems[:, [0, 1]],
                      self.elems[:, [1, 2]],
                      self.elems[:, [0, 2]]]
 
-    @property
+    @cached_property
     def edge_to_node(self) -> np.ndarray:
         """A matrix of edges. Each row contains the start and end node tags.
         Duplicate elements are removed and edges are sorted."""
@@ -129,7 +130,7 @@ class Mesh:
         by = p_k[1] - p_i[1]
         return 0.5 * abs(ax * by - ay * bx)
 
-    @property
+    @cached_property
     def elem_areas(self):
         """A vector of areas for the triangle elements."""
         areas = np.zeros(self.num_elems)
@@ -148,7 +149,7 @@ class Mesh:
         c: float = x_k - x_j
         return a, b, c
 
-    @property
+    @cached_property
     def coeffs(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """A tuple of (E,3) matrices with the coefficients a,b,c for the shape functions."""
         a = np.zeros([self.num_elems, 3])
--- exercise_1/mssolution.py
+++ exercise_1/mssolution.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
+from functools import cached_property
 from typing import Tuple
 
 import numpy as np
 from scipy.sparse import spmatrix
 import scipy.sparse.linalg as las
 
-from exercise_1.constants import GND, l_z
+from exercise_1.constants import GND, l_z, I
 from exercise_1.geometry import Geo
 from exercise_1.knu_matrix import Knu
 from exercise_1.load_vector import j_grid, X
@@ -19,14 +20,25 @@ class MSSolution:
 
     mesh: Mesh
     geo: Geo
-    knu: spmatrix = field(init=False)
-    j: spmatrix = field(init=False)
     a: np.ndarray = field(init=False)
 
     def __post_init__(self):
-        self.knu = Knu(self.mesh, self.geo)
-        self.j = j_grid(self.mesh)
         self.a = np.zeros(self.j.shape[0])
+
+    @cached_property
+    def knu(self) -> spmatrix:
+        """The Knu matrix."""
+        return Knu(self.mesh, self.geo)
+
+    @cached_property
+    def X(self) -> spmatrix:
+        """The current distribution vector."""
+        return X(self.mesh)
+
+    @cached_property
+    def j(self) -> spmatrix:
+        """The grid current vector."""
+        return self.X * I
 
     @property
     def idx_dir(self):
@@ -48,7 +60,7 @@ class MSSolution:
         x: np.ndarray = las.spsolve(A, b)
         return inflate(self.a, x, self.idx_dof)
 
-    @property
+    @cached_property
     def b(self) -> np.ndarray:
         """The values for the magnetic flux density in x- and y- direction. Matrix of size (E,2)."""
         a_z = self.a[self.mesh.elems]
@@ -59,10 +71,9 @@ class MSSolution:
         by = -np.sum(b * a_z / S, axis=1) / (2 * l_z)
         return np.vstack([bx, by]).T
 
-    @property
+    @cached_property
     def L(self) -> spmatrix:
         """The inductance matrix L of size (1,1)."""
-        x = X(self.mesh)  # current distribution
-        A, b = deflate(self.knu, x, self.idx_dof)
+        A, b = deflate(self.knu, self.X, self.idx_dof)
         y: np.ndarray = las.spsolve(A, b)
-        return x.T * inflate(self.a, y, self.idx_dof)
+        return self.X.T * inflate(self.a, y, self.idx_dof)
